b6f63c37e5985a90c56ab38de60311c16d8aaa0a
zaabsyid <abdulrosyid2002@gmail.com>
2024-09-30 09:14:02 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'product_name' => 'required|max:150',
+            'category' => 'required|max:100',
+            'price' => 'required|numeric',
+            'discount' => 'nullable|numeric',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'product_name' => 'required|max:150',
+            'category' => 'required|max:100',
+            'price' => 'required|numeric',
+            'discount' => 'nullable|numeric',
+        ]);
+
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(null, 204);
+    }
+}
